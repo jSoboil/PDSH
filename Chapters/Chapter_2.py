c9f36de9698da7ad31f91b62d1dac26ffd0e89b9
jSoboil <soboil.joshua@gmail.com>
2021-12-06 16:13:51 +0200

    continued chapter 2

--- Chapters/Chapter_2.py
+++ Chapters/Chapter_2.py
@@ -43,3 +43,34 @@ np.array([1, 2, 3, 4], dtype = 'float32')
 # The nested lists result in multidimensional arrays...
 np.array([range(i, i + 3) for i in [2, 4, 6]])
 # The inner lists are treated as rows of the resulting two-dimensional array.
+
+## Creating Arrays from Scratch:
+# Especially for larger arrays, it is more efficient to create arrays from scratch
+# using routines built into NumPy. Below are several examples.
+
+# 1. A length-10 integer array filled with zeros
+np.zeros(10, dtype = int)
+
+# 2. A 3x5 floating-point array filled with 1s
+np.ones((3, 5), dtype = float)
+
+# 3. A 3x5 array filled with 3.14
+np.full((3, 5), 3.14)
+
+# 4. An array filled with a linear sequence starting at 0, ending at 20, stepping 
+# by 2 (which is similar to the standard range function)
+np.arange(0, 20, 2)
+
+# 5. An array of five values evenly spaced between 0 and 1
+np.linspace(0, 1, 5)
+
+# 6. A 3x3 array of uniformly distributed random values between 0, 1
+np.random.random((3, 3))
+
+# 7. A 3x3 array of normally distributed random values with mean 0 and sd 1
+np.random.normal(0, 1, (3, 3))
+
+# 8. A 3x3 I matrix
+np.eye(3)
+
+# NumPy Standard Data Types: 
