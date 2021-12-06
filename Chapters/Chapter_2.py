f78a680953cca1236c3ab4dd0acd94dcdf4cc59d
jSoboil <soboil.joshua@gmail.com>
2021-12-06 15:59:38 +0200

    continued chapter 2

--- Chapters/Chapter_2.py
+++ Chapters/Chapter_2.py
@@ -6,7 +6,7 @@ import numpy as np
 result = 0
 for i in range(100):
  result += i
-print(result)
+result
 
 # Exploring integers
 L = list(range(100))
--- env.R
+++ env.R
@@ -1,9 +1,5 @@
 # Load reticulate package:
 library(reticulate)
 
-# create a new environment/select venv:
-# virtualenv_create("pdsd_venv/")
-Sys.setenv(RETICULATE_PYTHON = "pdsd_venv/bin/python")
-
-# Install numpy to venv:
-# virtualenv_install("numpy")
+# Load virtual env:
+use_virtualenv("env.")
