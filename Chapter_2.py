b6d929b9176e2bcf8f4b347813c75f614770f984
jSoboil <soboil.joshua@gmail.com>
2021-12-06 15:11:34 +0200

    continued chapter 2

--- Chapter_2.py
+++ Chapter_2.py
@@ -1,2 +1,12 @@
-# Remember, before starting to use Python in R, you have to use the 
-# reticulate::repl_python() command, which loads a Python interpreter.
+# Chapter 2:
+# Note: before starting Python in R, you have to use the reticulate::repl_python() 
+# command, which loads a Python interpreter.
+
+# Chapter 2 outlines techniques for effectively loading, storing, and manipulating
+# in-memory data in Python.
+import numpy as np
+
+result = 0
+for i in range(100):
+ result += i
+print(result)
--- env.R
+++ env.R
@@ -1,9 +1,9 @@
+# Load reticulate package:
 library(reticulate)
-# create a new environment 
+
+# create a new environment/select venv:
 # virtualenv_create("pdsd_venv/")
 Sys.setenv(RETICULATE_PYTHON = "pdsd_venv/bin/python")
-# install SciPy
-# virtualenv_install("numpy")
 
-# import SciPy (it will be automatically discovered in "r-reticulate")
-# scipy <- import("scipy")
+# Install numpy to venv:
+# virtualenv_install("numpy")
