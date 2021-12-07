124cd413073cb2c36f225a25614745fbbccde300
jSoboil <soboil.joshua@gmail.com>
2021-12-07 14:52:06 +0200

    continued chapter 2

--- Chapters/Chapter_2.py
+++ Chapters/Chapter_2.py
@@ -73,4 +73,61 @@ np.random.normal(0, 1, (3, 3))
 # 8. A 3x3 I matrix
 np.eye(3)
 
-# NumPy Standard Data Types: 
+# The Basics of NumPy Arrays:
+# This section presents several examples using NumPy array manipulation to access
+# data and subarrays, and to split, reshape, and join the arrays.
+
+## NumPy Attributes:
+# We’ll start by defining three random arrays: a one-dimensional, two-dimensional, 
+# and three-dimensional array. We’ll use NumPy’s random number generator, which
+# we will seed with a set value in order to ensure that the same random arrays
+# are generated each time this code is run
+np.random.seed(0)
+
+# One-dimensional array
+x_1 = np.random.randint(10, size = 6)
+# Two-dimensional array
+x_2 = np.random.randint(10, size = (3, 4))
+# Three-dimensional array
+x_3 = np.random.randint(10, size = (3, 4, 5))
+
+# Each array has attributes ndim (the number of dimensions), shape (the size of
+# each dimension), and size (the total size of the array).
+print("x_3 ndim: ", x_3.ndim)
+print("x_3 shape: ", x_3.shape)
+print("x_3 size: ", x_3.size)
+
+## Array Indexing - Accessing Single Elements:
+# Much the same as standard R. But important to note that unlike Python lists, 
+# NumPy arrays have a fixed type. This means, for example, that if you attempt 
+# to insert a floating-point value to an integer array, the value will be 
+# silently truncated. Don’t be caught unaware by this behavior!
+x_1[0] = 3.14159 # this is silently truncated!
+
+## Reshaping of Arrays:
+# Another useful type of operation is reshaping of arrays. The most flexible way
+# of doing this is with the reshape() method. For example, if you want to put 
+# the numbers 1 through 9 in a 3×3 grid, you can do the following.
+grid = np.arange(1, 10).reshape((3, 3))
+print(grid)
+# Note that for this to work, the size of the initial array must match the size
+# of the reshaped array. Where possible, the reshape method will use a no-copy
+# view of the initial array, but with noncontiguous memory buffers this is not 
+# always the case.
+
+# Another common reshaping pattern is the conversion of a one-dimensional array 
+# into a two-dimensional row or column matrix. You can do this with the reshape
+# method, or more easily by making use of the newaxis keyword within a slice 
+# operation.
+x = np.array([1, 2, 3])
+# Reshape into ropw vector via reshape
+x.reshape((1, 3))
+# Row vector via newaxis
+x[np.newaxis, :]
+# Column vector via reshape
+x.reshape((3, 1))
+# Column vector via newaxis
+x[ : , np.newaxis]
+
+## Array Concatenation and Splitting:
+### Concatenation of arrays:
--- env.R
+++ env.R
@@ -1,5 +1,9 @@
+# Activating virtual env. -------------------------------------------------
 # Load reticulate package:
 library(reticulate)
-
 # Load virtual env:
 use_virtualenv("env.")
+# Point reticulate to Python version of choice:
+Sys.setenv(RETICULATE_PYTHON = "env./bin/python3.9")
+
+# End file ----------------------------------------------------------------
