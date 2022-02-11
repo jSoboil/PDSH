126fac641362c39313fa483c9f46b095ccd38577
jSoboil <soboil.joshua@gmail.com>
2022-02-11 12:10:58 +0200

    continued chapter 5

--- chapters/chapter_5.py
+++ chapters/chapter_5.py
@@ -1403,7 +1403,7 @@ def make_hello(N = 1000, rseed = 42):
  
  # Open PNG and draw random points on it...
  from matplotlib.image import imread
- data = imread("hello.png")[::-1, :, 0].T
+ data = imread("img/hello.png")[::-1, :, 0].T
  rng = np.random.RandomState(rseed)
  X = rng.rand(4 * N, 2)
  i, j = (X * data.shape).astype(int).T
@@ -1521,3 +1521,134 @@ plt.clf()
 # MDS, the quantity preserved is the distance between every pair of points.
 
 ##### Nonlinear Embeddings: Where MDS Fails
+# Where MDS breaks down is when the embedding is nonlinear—that is, when it goes 
+# beyond this simple set of operations. Consider the following embedding, which 
+# takes the input and contorts it into an “S” shape in three dimensions.
+def make_hello_s_curve(X):
+ t = (X[:, 0] - 2) * 0.75 * np.pi
+ x = np.sin(t)
+ y = X[:, 1]
+ z = np.sign(t) * (np.cos(t) - 1)
+ return np.vstack((x, y, z)).T
+
+X_S = make_hello_s_curve(X)
+# This is again three-dimensional data, but we can see that the embedding is 
+# much more complicated...
+from mpl_toolkits import mplot3d
+ax = plt.axes(projection = "3d")
+ax.scatter3D(X_S[:, 0], X_S[:, 1], X_S[:, 2], 
+             **colourise);
+plt.show()
+plt.clf()
+# If we try a simple MDS algorithm on this data, it is not able to “unwrap” this 
+# nonlinear embedding, and we lose track of the fundamental relationships in the 
+# embedded manifold.
+from sklearn.manifold import MDS
+model = MDS(n_components = 2, random_state = 2)
+out_S = model.fit_transform(X_S)
+plt.scatter(out_S[:, 0], out_S[:, 1], **colourise)
+plt.axis("equal");
+plt.show()
+plt.clf()
+# The best two-dimensional linear embedding does not unwrap the S-curve, but 
+# instead throws out the original y-axis.
+
+##### Nonlinear Manifolds: Locally Linear Embedding
+# We can see that the source of the problem is that MDS tries to preserve 
+# distances between faraway points when constructing the embedding. But what if 
+# we instead modified the algorithm such that it only preserves distances 
+# between nearby points? The resulting embedding would be closer to what we want.
+
+# Rather than preserving all distances, it instead tries to preserve only the 
+# distances between neighboring points - in this case, the nearest 100 neighbors 
+# of each point.
+
+# LLE comes in a number of flavors; here we will use the modified LLE algorithm 
+# to recover the embedded two-dimensional manifold. In general, modified LLE does
+# better than other flavors of the algorithm at recovering well-defined manifolds
+# with very little distortion.
+from sklearn.manifold import LocallyLinearEmbedding
+model = LocallyLinearEmbedding(n_neighbors = 100, n_components = 2, 
+                               method = "modified", eigen_solver = "dense")
+out = model.fit_transform(X_S)
+# plot data
+fig, ax = plt.subplots()
+ax.scatter(out[:, 0], out[:, 1], **colourise)
+ax.set_ylim(0.15, -0.15);
+plt.show()
+plt.clf()
+# The result remains somewhat distorted compared to our original manifold, but 
+# captures the essential relationships in the data!
+
+##### Example: Isomap on Faces
+# One place manifold learning is often used is in understanding the relationship 
+# between high-dimensional data points. Here let’s apply Isomap on some faces 
+# data.
+from sklearn.datasets import fetch_lfw_people
+faces = fetch_lfw_people(min_faces_per_person = 30)
+faces.data.shape
+
+# plot data
+fig, ax = plt.subplots(4, 8, subplot_kw = dict(xticks = [], yticks = []))
+for i, axi in enumerate(ax.flat):
+ axi.imshow(faces.images[i], cmap = "gray")
+plt.show();
+plt.clf()
+# We would like to plot a low-dimensional embedding of the 2,914-dimensional data
+# to learn the fundamental relationships between the images. One useful way to 
+# start is to compute a PCA, and examine the explained variance ratio, which 
+# will give us an idea of how many linear features are required to describe the 
+# data.
+from sklearn.decomposition import PCA
+model = PCA(n_components = 100, svd_solver = "randomized", whiten = True).fit(faces.data)
+plt.plot(np.cumsum(model.explained_variance_ratio_))
+plt.xlabel("n components")
+plt.ylabel("cumulative variance");
+plt.show()
+plt.clf()
+# We see that for this data, nearly 100 components are required to preserve 90% 
+# of the variance. This tells us that the data is intrinsically very high
+# dimensional — it can’t be described linearly with just a few components.
+
+# When this is the case, nonlinear manifold embeddings like LLE and Isomap can be
+# helpful. We can compute an Isomap embedding on these faces using the same 
+# pattern shown before...
+from sklearn.manifold import Isomap
+model = Isomap(n_components = 2)
+proj = model.fit_transform(faces.data)
+proj.shape
+
+# The output is a two-dimensional projection of all the input images. To get a 
+# better idea of what the projection tells us, let’s define a function that will 
+# output image thumbnails at the locations of the projections.
+from matplotlib import offsetbox
+def plot_components(data, model, images = None, ax = None, 
+                    thumb_frac = 0.05, cmap = "gray"):
+    ax = ax or plt.gca()
+    
+    proj = model.fit_transform(data)
+    ax.plot(proj[:, 0], proj[:, 1], ".k")
+    
+    if images is not None:
+     min_dist_2 = (thumb_frac * max(proj.max(0) - proj.min(0))) ** 2
+     shown_images = np.array([2 * proj.max(0)])
+     for i in range(data.shape[0]):
+      dist = np.sum((proj[i] - shown_images) ** 2, 1)
+      if np.min(dist) < min_dist_2:
+       # don't show points that are too close
+       continue
+      
+      shown_images = np.vstack([shown_images, proj[i]])
+      imagebox = offsetbox.AnnotationBbox(
+        offsetbox.OffsetImage(images[i], cmap = cmap),
+                                         proj[i])
+      ax.add_artist(imagebox)
+
+# Call function and plot...
+fig, ax = plt.subplots(figsize = (10, 10))
+plot_components(faces.data, model = Isomap(n_components = 2),
+                images = faces.images[:, ::2, ::2]);
+plt.show()
+plt.clf()
+
+### In Depth: k-Means Clustering
